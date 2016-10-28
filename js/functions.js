7f1c8f797c8c2e2b7a54a23325dcb10e58df08db
tabuz <zelek.hubert@gmail.com>
2016-10-28 15:36:29 +0100

    Highcharts styling, stats++

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,32 @@
+var chartCardio,
+    chartHeartrate,
+    chartDistance;
 $( document ).ready(function() {
+      chartCardio = Highcharts.chart('chart-cardio', {
 
-      var myChart = Highcharts.chart('chart', {
-
-          chart: {type: 'areaspline',
+            chart: {type: 'areaspline',
                   spacing: [0 ,0 ,0 ,0],
                   margin: [100, -50, 0, -50]},
-          legend: {enabled: false},
-          title: {text: null},
+            legend: {enabled: false},
+            title: {text: null},
 
-          xAxis: {categories: [0, 1, 2, 3, 4, 5, 6, 7],
+            xAxis: {categories: [0, 1, 2, 3, 4, 5, 6, 7, 8],
                   offset: 0},
 
-          yAxis: {endOnTick: false,
+            yAxis: {endOnTick: false,
                   tickAmount: 3,
                   max: 900,
                   title: {text: null},
                   offset: -150,
                   zIndex: -100},
-          tooltip: {
+            tooltip: {
             useHTML: true,
             borderColor: 'transparent',
             formatter: function() {
-                if (this.x > 0) {
-                    var s = '<span class="tooltip-value">'+this.y+'</span>';
+                if (this.x > 0 && this.x < 8) {
+                    var s = '<div class="tooltip-kcal"><span class="tooltip-value">'+this.y+'</span><span class="tooltip-desc">KCAL<br />OCT, '+this.x+'</span></div>';
+                    $('.stat-kcal span').html(this.y);
+                    $('.stat-day span').html(this.x);
                     return s;
                 }
                 else return false;
@@ -38,11 +42,16 @@ $( document ).ready(function() {
                         hover: {
                             fillColor: '#e9ac8f',
                             lineColor: 'rgba(0,0,0, .6)',
-                            radius: 4,
-                            lineWidth: 4
+                            radius: 5,
+                            lineWidth: 5
                         }
-                    }
+                        }
                         },
+                states: {
+                      hover:{
+                          halo: false
+                      }
+                },
                 lineColor: '#e9ac8f'}
                        },
           series: [{
@@ -58,8 +67,171 @@ $( document ).ready(function() {
                    {y: 452},
                    {y: 350},
                    {y: 300},
-                   {y: 220}
+                   {y: 220},
+                   {y: 220,
+                     marker: {enabled: false,
+                     states:{
+                       hover:{enabled: false},
+                       select:{enabled: false}
+                       }}}
                   ]
         }]
+      });
+      chartHeartrate = Highcharts.chart('chart-heartrate', {
+
+          chart: {type: 'areaspline',
+                  spacing: [0 ,0 ,0 ,0],
+                  margin: [100, -50, 0, -50]},
+          legend: {enabled: false},
+          title: {text: null},
+
+          xAxis: {categories: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  offset: 0},
+
+          yAxis: {endOnTick: false,
+                  tickAmount: 3,
+                  max: 150,
+                  title: {text: null},
+                  offset: -150,
+                  zIndex: -100},
+          tooltip: {
+            useHTML: true,
+            borderColor: 'transparent',
+            formatter: function() {
+                if (this.x > 0 && this.x < 8) {
+                    var s = '<div class="tooltip-kcal"><span class="tooltip-value">'+this.y+'</span><span class="tooltip-desc">/ MIN<br />OCT, '+this.x+'</span></div>';
+                    $('.stat-heartrate span').html(this.y);
+                    $('.stat-day span').html(this.x);
+                    return s;
+                }
+                else return false;
+                }
+              },
+          plotOptions: {
+              series: {
+                marker: {enabled: false,
+                        fillColor: '#FFFFFF',
+                        lineWidth: 2,
+                        lineColor: null,
+                        states: {
+                        hover: {
+                            fillColor: '#e9ac8f',
+                            lineColor: 'rgba(0,0,0, .6)',
+                            radius: 5,
+                            lineWidth: 5
+                        }
+                        }
+                        },
+                states: {
+                      hover:{
+                          halo: false
+                      }
+                },
+                lineColor: '#e9ac8f'}
+                       },
+          series: [{
+            data: [{y: 110,
+                      marker: {enabled: false,
+                      states:{
+                        hover:{enabled: false},
+                        select:{enabled: false}
+                      }}},
+                   {y: 120},
+                   {y: 125},
+                   {y: 118},
+                   {y: 119},
+                   {y: 122},
+                   {y: 118},
+                   {y: 120},
+                   {y: 120,
+                     marker: {enabled: false,
+                     states:{
+                       hover:{enabled: false},
+                       select:{enabled: false}
+                       }}}
+                  ]
+        }]
+      });
+      chartDistance = Highcharts.chart('chart-distance', {
+
+          chart: {type: 'areaspline',
+                  spacing: [0 ,0 ,0 ,0],
+                  margin: [100, -50, 0, -50]},
+          legend: {enabled: false},
+          title: {text: null},
+
+          xAxis: {categories: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  offset: 0},
+
+          yAxis: {endOnTick: false,
+                  tickAmount: 3,
+                  max: 10,
+                  title: {text: null},
+                  offset: -150,
+                  zIndex: -100},
+          tooltip: {
+            useHTML: true,
+            borderColor: 'transparent',
+            formatter: function() {
+                if (this.x > 0 && this.x < 8) {
+                    var s = '<div class="tooltip-kcal"><span class="tooltip-value">'+this.y+'</span><span class="tooltip-desc">KM<br />OCT, '+this.x+'</span></div>';
+                    $('.stat-distance span').html(this.y);
+                    $('.stat-day span').html(this.x);
+                    return s;
+                }
+                else return false;
+                }
+              },
+          plotOptions: {
+              series: {
+                marker: {enabled: false,
+                        fillColor: '#FFFFFF',
+                        lineWidth: 2,
+                        lineColor: null,
+                        states: {
+                        hover: {
+                            fillColor: '#e9ac8f',
+                            lineColor: 'rgba(0,0,0, .6)',
+                            radius: 5,
+                            lineWidth: 5
+                        }
+                        }
+                        },
+                states: {
+                      hover:{
+                          halo: false
+                      }
+                },
+                lineColor: '#e9ac8f'}
+                       },
+          series: [{
+            data: [{y: 2.54,
+                      marker: {enabled: false,
+                      states:{
+                        hover:{enabled: false},
+                        select:{enabled: false}
+                      }}},
+                   {y: 3.14},
+                   {y: 3.58},
+                   {y: 4.20},
+                   {y: 5.5},
+                   {y: 4.28},
+                   {y: 3.56},
+                   {y: 3.10},
+                   {y: 2.9,
+                     marker: {enabled: false,
+                     states:{
+                       hover:{enabled: false},
+                       select:{enabled: false}
+                       }}}
+                  ]
+        }]
+      });
+
+      $('#option-toggle').change(function(){
+        var value = $(this).val();
+        $('[id^="chart-"]').removeClass('chart-active');
+        $('#chart-'+value).addClass('chart-active');
+
       });
 });
